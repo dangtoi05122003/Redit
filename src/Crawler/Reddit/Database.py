567b28625e5d2515f20a309b0232a053f1276a05
dangtoi05122003 <dangtoi05122003@gmail.com>
2025-09-24 09:17:52 +0700

    Init

---
+++ src/Crawler/Reddit/Database.py
@@ -0,0 +1,6 @@
+import os
+MONGO_URI = os.getenv("MONGO_URI")
+MONGO_DB = os.getenv("MONGO_DB")
+MONGO_COLLECTION = os.getenv("MONGO_COLLECTION")
+client_id=os.getenv("CLIENT_ID")
+client_secret=os.getenv("CLIENT_SECRET")
