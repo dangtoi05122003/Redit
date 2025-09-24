9410885cc0fa79c98114fa61fd35ed4fc80cc2f6
dangtoi05122003 <dangtoi05122003@gmail.com>
2025-09-24 09:17:17 +0700

    Fix

--- src/Crawler/VnExpress/Database.py
+++
@@ -1,4 +0,0 @@
-MONGO_URI = "mongodb+srv://dangtoi:123@datascience.3ef7kfw.mongodb.net/"
-MONGO_DB = "VnExpress"
-MONGO_COLLECTION = "Data"
-MONGO_COLLECTION_PROCESSED = "process"
--- src/Crawler/VnExpress/config.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.chrome.options import Options
-
-def setup():
-    options = Options()
-    options.add_argument("--blink-settings=imagesEnabled=false")
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    return options
